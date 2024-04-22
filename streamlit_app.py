4ca8ba7d22a83509eb337f9443b7eaa027090e07
sihamnimale <98288724+sihamnimale@users.noreply.github.com>
2024-04-22 20:21:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,19 @@ ingredients_list = st.multiselect(
   )
 
 if ingredients_list:
-        ingredients_string = ''
-        
-        for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ' '
-            st.subheader(fruit_chosen + ' Nutrition Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_chosen)
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    ingredients_string = ''
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-            #st.write(ingredients_string)
-        
-            my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                        values ('""" + ingredients_string + """', '""" +name_on_order+ """')"""
-            
-            #st.write(my_insert_stmt)
-            #st.stop()
-            
-            time_to_insert = st.button('Submit Order')
-            
-            if time_to_insert:
-               session.sql(my_insert_stmt).collect()
-               st.success('Your Smoothie is ordered!', icon="✅")
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
